1ad8372f7720fab509daac163d59d5f78dd4bd32
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2024-02-24 19:16:13 +0530

    revamp

--- js/script.js
+++ js/script.js
@@ -1,27 +1,190 @@
-function updateCountdown() {
-    const countdownElement = document.getElementById("countdown");
-    const countDownDate = new Date("April 20, 2024 00:00:00").getTime();
-    const intervalId = setInterval(updateDisplay, 1000);
-  
-    function updateDisplay() {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-  
-      if (distance < 0) {
-        clearInterval(intervalId);
-        countdownElement.textContent = "MUN Open!";
-        return;
-      }
-  
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-      const formattedCountdown = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-      countdownElement.textContent = formattedCountdown;
-    }
+window.onload = async () => {
+  document.getElementsByClassName('dropdown')[0].style.left = document.getElementById('homeLink').getBoundingClientRect().left + 'px';
+
+  try {
+    const response = await fetch("https://indiamun-backend.onrender.com/chalja/12ka4", {
+      method: "GET"
+    });
+    console.log("backend initiated");
+  } catch (error) {
+    console.log(error);
   }
-  
-  updateCountdown();  
-  
+};
+
+document.body.onclick = (e) => {
+  if (e.target !== document.getElementsByClassName('dropdown')[0]) {
+    document.getElementsByClassName('dropdown')[0].style.transform = 'scale(1, 0)';
+  }
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
+
+async function sendData(e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const text = document.getElementById("text").value;
+  if (!email || !subject || !text) {
+    return alert("Please enter all the details.");
+  }
+
+  const data = {
+    "userEmail": email,
+    "subject": subject,
+    "text": text
+  };
+
+  try {
+    const response = await fetch("https://indiamun-backend.onrender.com/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(JSON.stringify(data));
+    console.log("success");
+    alert("Emails sent successfully. Check your inbox for confirmation");
+  } catch (error) {
+    console.log(error);
+    alert("There was an error in sending the email. Please try again later.");
+  }
+}
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.getElementsByClassName("specialText").onmouseover = event => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
+
+const hamburger = document.getElementById("hamburger");
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+hamburger.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (hamburgerMenu.classList.contains("phone-nav-menu-visible")) {
+    hamburgerMenu.classList.add("phone-nav-menu-hidden");
+    hamburgerMenu.classList.remove("phone-nav-menu-visible");
+  } else {
+    hamburgerMenu.classList.add("phone-nav-menu-visible");
+  }
+});
+
+let start = new Date().getTime();
+
+const originPosition = { x: 0, y: 0 };
+
+const last = {
+  starTimestamp: start,
+  starPosition: originPosition,
+  mousePosition: originPosition
+};
+
+const config = {
+  starAnimationDuration: 1500,
+  minimumTimeBetweenStars: 250,
+  minimumDistanceBetweenStars: 75,
+  colors: ["249 146 253", "252 254 255"],
+  sizes: ["1.4rem", "1rem", "0.6rem"],
+  animations: ["fall-1", "fall-2", "fall-3"]
+};
+
+let count = 0;
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+  selectRandom = items => items[rand(0, items.length - 1)];
+
+const withUnit = (value, unit) => `${value}${unit}`,
+  px = value => withUnit(value, "px"),
+  ms = value => withUnit(value, "ms");
+
+const calcDistance = (a, b) => {
+  const diffX = b.x - a.x,
+    diffY = b.y - a.y;
+
+  return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+};
+
+const calcElapsedTime = (start, end) => end - start;
+
+const appendElement = element => document.body.appendChild(element),
+  removeElement = (element, delay) => setTimeout(() => document.body.removeChild(element), delay);
+
+const createStar = position => {
+  const star = document.createElement("span"),
+    color = selectRandom(config.colors);
+
+  star.className = "star fa-solid fa-star";
+
+  star.style.left = px(position.x);
+  star.style.top = px(position.y);
+  star.style.fontSize = selectRandom(config.sizes);
+  star.style.color = `rgb(${color})`;
+  star.style.textShadow = `0px 0px 1.5rem rgb(${color} / 0.5)`;
+  star.style.animationName = config.animations[count++ % 3];
+  star.style.starAnimationDuration = ms(config.starAnimationDuration);
+
+  appendElement(star);
+
+  removeElement(star, config.starAnimationDuration);
+};
+
+const handleOnMove = e => {
+  const mousePosition = { x: e.pageX, y: e.pageY };
+
+  const now = new Date().getTime(),
+    hasMovedFarEnough = calcDistance(last.starPosition, mousePosition) >= config.minimumDistanceBetweenStars,
+    hasBeenLongEnough = calcElapsedTime(last.starTimestamp, now) > config.minimumTimeBetweenStars;
+
+  if (hasMovedFarEnough || hasBeenLongEnough) {
+    createStar(mousePosition);
+
+    last.starTimestamp = now;
+    last.starPosition = mousePosition;
+  }
+
+  last.mousePosition = mousePosition;
+};
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+document.body.onmouseleave = () => last.mousePosition = originPosition;
+
+function showInternalLinks() {
+  document.getElementsByClassName('dropdown')[0].style.transform = 'scale(1, 1)';
+}
