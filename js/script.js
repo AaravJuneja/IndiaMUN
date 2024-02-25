af0dc0900952b627a660b1adec233caac1ee944f
Sniqr <itsayushgamez@gmail.com>
2024-02-25 15:49:51 +0530

    push 82
    added smooth scroll
    bug fixes
    imgs

--- js/script.js
+++ js/script.js
@@ -117,3 +117,23 @@ document.getElementsByClassName("specialText")[0].onmouseover = event => {
     iteration += 1 / 3;
   }, 30);
 };
+
+//ayush ka kaam hai
+
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 100, // could be -ve 
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({ 
+  behavior: 'smooth' 
+});
